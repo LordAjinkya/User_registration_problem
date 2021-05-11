013482a4805f456c090dc9cd2894ce0cfd981193
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-11 12:05:05 +0530

    Added:code to validate first name of user

--- main/java/com/registration/Registration_user.java
+++ main/java/com/registration/Registration_user.java
@@ -1,8 +1,27 @@
 package com.registration;
+import java.util.*;
 
 public class Registration_user {
+    public static final Scanner scan = new Scanner(System.in);
+    public static void firstname()
+    {
+        System.out.println("Enter Your First name");
+        String name = scan.nextLine();
+
+        System.out.println("The first name is: " + name);
+        String pattern = "^([A-Z]{1})([a-z]{2,})$";
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to user registration problem");
+        firstname();
 
     }
 }
