e93186ffafb9470e4e070316e86f3e7f2acaa764
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-11 12:01:23 +0530

    Added: welcome msg

---
+++ main/java/com/registration/Registration_user.java
@@ -0,0 +1,8 @@
+package com.registration;
+
+public class Registration_user {
+    public static void main(String[] args) {
+        System.out.println("Welcome to user registration problem");
+
+    }
+}
