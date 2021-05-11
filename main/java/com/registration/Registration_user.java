2abb99f4c4146bc035dc23b9d95c8cb86447c6d6
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-05-11 12:09:40 +0530

    Added: code to validate lastname of user

--- main/java/com/registration/Registration_user.java
+++ main/java/com/registration/Registration_user.java
@@ -19,9 +19,28 @@ public class Registration_user {
             System.out.println("Name is Invalid");
         }
     }
+
+    //validating last name of the user
+    public static void LastName()
+    {
+        System.out.println("Enter Your Last name");
+        String name = scan.nextLine();
+
+        System.out.println("The Last name is: " + name);
+        String pattern = "^([A-Z]{1})([a-z]{2,})$";
+        if( name.matches(pattern))
+        {
+            System.out.println("Name is Valid");
+        }
+        else
+        {
+            System.out.println("Name is Invalid");
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to user registration problem");
         firstname();
+        LastName();
 
     }
 }
